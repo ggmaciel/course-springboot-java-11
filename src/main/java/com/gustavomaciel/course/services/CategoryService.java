47218d548fb1618557c020519f8d52bb1ab55632
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-09-08 11:36:03 -0300

    Category entity

---
+++ src/main/java/com/gustavomaciel/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.gustavomaciel.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavomaciel.course.entities.Category;
+import com.gustavomaciel.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> category = repository.findById(id);
+		
+		return category.get();
+	}
+}
